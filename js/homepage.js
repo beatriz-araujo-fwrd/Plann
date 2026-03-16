b293da55861988b99d72001862eeb5da7e4f6513
beatriz-araujo <beatriz@forwwward.studio>
2026-03-16 09:17:07 +0000

    Added homepage interactive team section

--- js/homepage.js
+++ js/homepage.js
@@ -73,5 +73,61 @@ export function homepage() {
         }
     }
 
+    // TEAM SECTION - text animation
+    const teamSection = document.querySelector('.section_hp_team.hp_team_card_effect');
+    if (teamSection) {
+        setTimeout(() => {
+            const container = document.querySelector('.hp_team_list_container')
+            const cardsContainer = container.querySelector('.hp_team_list_cards')
+            const cards = document.querySelectorAll('.hp_team_list_card')
+            const distance = cardsContainer.clientWidth - window.innerWidth
+
+            const scrollTween = gsap.to(cardsContainer, {
+                x: - distance,
+                ease: 'none', // linear progression
+                // let's pin our container while our cardsContainer is animating
+                scrollTrigger: {
+                    trigger: teamSection,
+                    // pin: true,
+                    scrub: true, // progress with the scroll
+                    start: 'top 85%',
+                    // end: '+=' + distance
+                    end: 'bottom top'
+                }
+            });
+
+            cards.forEach(card => {
+                const values = {
+                    // get a value between 30 and 50 or -30 and -50
+                    x: (Math.random() * 20 + 30) * (Math.random() < 0.5 ? 1 : -1),
+                    // get a value between 10 and 16 or -16 and -10
+                    y: (Math.random() * 6 + 10) * (Math.random() < 0.5 ? 1 : -1),
+                    // get a value between 10 and 20 or -10 and -20
+                    rotation: (Math.random() * 10 + 10) * (Math.random() < 0.5 ? 1 : -1)
+                };
+
+                gsap.fromTo(card, {
+                    // let's start from this 3 values
+                    rotation: values.rotation,
+                    xPercent: values.x,
+                    yPercent: values.y
+                }, {
+                    // and finish to its 3 opposite values
+                    rotation: - values.rotation,
+                    xPercent: - values.x,
+                    yPercent: - values.y,
+                    ease: 'none', // linear progression
+                    scrollTrigger: {
+                        trigger: card,
+                        containerAnimation: scrollTween, // our tween will listen to our scrollTween container position
+                        start: 'left 120%',
+                        end: 'right -20%',
+                        scrub: true, // the animation progress with the scroll
+                    }
+                })
+            })
+        }, 200);
+    }
+
     console.log("running homepage()");
 }
